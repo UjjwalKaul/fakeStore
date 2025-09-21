48f2236fc27dd21bcca912f245d1ef24d5bf1ab3
UjjwalKaul <ujjwal171@outlook.com>
2025-09-22 01:14:30 +0530

    Create Constants object and AppModule for ProductApi

---
+++ app/src/main/java/com/ujjwal/fakestore/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.ujjwal.fakestore.di
+
+import com.ujjwal.fakestore.store.data.ProductApi
+import com.ujjwal.fakestore.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn
+@Module
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesProductsApi(): ProductApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ProductApi::class.java)
+    }
+}
