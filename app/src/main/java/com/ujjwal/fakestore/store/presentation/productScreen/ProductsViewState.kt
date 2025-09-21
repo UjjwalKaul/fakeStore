9fa540d3b1aa85f1fddf5fad14c71e9c4a5e9ff9
UjjwalKaul <ujjwal171@outlook.com>
2025-09-22 01:00:29 +0530

    Refactor ProductsViewState to data class

--- app/src/main/java/com/ujjwal/fakestore/store/presentation/productScreen/ProductsViewState.kt
+++ app/src/main/java/com/ujjwal/fakestore/store/presentation/productScreen/ProductsViewState.kt
@@ -2,7 +2,7 @@ package com.ujjwal.fakestore.store.presentation.productScreen
 
 import com.ujjwal.fakestore.store.domain.model.Product
 
-sealed class ProductsViewState(
+data class ProductsViewState(
     val isLoading: Boolean = false,
     val products: List<Product> = emptyList(),
     val error: String? = null
