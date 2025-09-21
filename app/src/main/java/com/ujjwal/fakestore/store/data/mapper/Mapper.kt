52b14f6b11cdb39b1cf401f7246ba0b59b574891
UjjwalKaul <ujjwal171@outlook.com>
2025-09-22 00:29:29 +0530

    Create extension function to map Throwable to NetworkError

---
+++ app/src/main/java/com/ujjwal/fakestore/store/data/mapper/Mapper.kt
@@ -0,0 +1,15 @@
+package com.ujjwal.fakestore.store.data.mapper
+
+import coil.network.HttpException
+import com.ujjwal.fakestore.store.domain.model.ApiError
+import com.ujjwal.fakestore.store.domain.model.NetworkError
+import okio.IOException
+
+fun Throwable.toNetworkError(): NetworkError {
+    val error = when (this) {
+        is IOException -> ApiError.NetworkError
+        is HttpException -> ApiError.UnknownResponse
+        else -> ApiError.UnknownError
+    }
+    return NetworkError(error = error, t = this)
+}
